77e7d65cab7551048c59641961cae34f84e3b732
Beatriz Camille <biacamille2019@gmail.com>
2021-04-12 08:17:36 -0300

    :tada: Challenge finalized

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,35 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if (!newTaskTitle) return;
+
+    const newTask = {
+      id: Math.random(),
+      title: newTaskTitle,
+      isComplete: false
+    }
+
+    setTasks (oldState => [...oldState, newTask]);
+    setNewTaskTitle ('');
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const newTasks = tasks.map (task => task.id == id ? {
+      ...task,
+      isComplete: !task.isComplete
+    }: task);
+
+    setTasks(newTasks)
   }
+
+
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+
+    const filteredTask = tasks.filter (task => task.id != id);
+    setTasks(filteredTask)
   }
 
   return (
